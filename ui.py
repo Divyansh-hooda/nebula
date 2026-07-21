7b843d1ceb76c33432efcb622ec5dcf58b3485aa
Divyansh-hooda <150668190+Divyansh-hooda@users.noreply.github.com>
2026-07-21 18:26:19 +0000

    modified:   ui.py

--- ui.py
+++ ui.py
@@ -3,3 +3,20 @@ import sys
 import time
 import shutil
 import subprocess
+import tkinter as tk
+import threading
+from queue import Queue
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import simpledialog
+import config
+import database
+import utils
+class Nebula:
+    def __init__(self, root, clipboard):
+        self.root = root
+        self.clipboard = clipboard
+        self.current = os.path.expanduser("~")
+        self.history = []
+        self.selected = None
+        self.favorites = database.get_bookmarks()
