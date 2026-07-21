7c1c70c9a8eb1632be4ae86df49b9f59e1968903
Divyansh-hooda <150668190+Divyansh-hooda@users.noreply.github.com>
2026-07-21 13:43:13 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 import config
 import database
 from ui import Nebula
+from clipboard_manager import ClipboardManager
 
 os.makedirs(config.VAULT_DIR,exist_ok=True)
 os.makedirs(config.LOG_DIR,exist_ok=True)
 database.initialize()
+clipboard = ClipboardManager()
 root=tk.Tk()
-app=Nebula(root)
+app=Nebula(root, clipboard)
 root.mainloop()
