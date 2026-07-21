f73fc3f10df26ebf828dc758eb747540e43691f2
Divyansh-hooda <150668190+Divyansh-hooda@users.noreply.github.com>
2026-07-21 09:04:12 +0000

    modified:   config.py

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ APP_NAME = "Nebula"
 WINDOW_WIDTH = 1350
 WINDOW_HEIGHT = 820
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_DIR = os.path.join(BASE_DIR, "database")
+DATABASE_FILE = os.path.join(DATABASE_DIR, "nebula.db")
+VAULT_DIR = os.path.join(BASE_DIR, "vault")
