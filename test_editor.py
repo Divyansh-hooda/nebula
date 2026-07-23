366b9362ce8f977625d79b19d9e9d684849b2957
Divyansh-hooda <150668190+Divyansh-hooda@users.noreply.github.com>
2026-07-23 18:31:23 +0000

    new file:   test_editor.py

---
+++ test_editor.py
@@ -0,0 +1,11 @@
+# this file is just to check if the text_editor.py is working correctly or not
+
+import tkinter as tk
+from text_editor import TextEditor
+
+root = tk.Tk()
+root.withdraw()
+
+TextEditor(root)
+
+root.mainloop()
