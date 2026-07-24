40794ab37adb6f40a6231381cd2a053356e7a894
codesterDiv <180194888+codesterDiv@users.noreply.github.com>
2026-07-24 10:37:30 +0530

    modified:   text_editor_v2.py

--- text_editor_v2.py
+++ text_editor_v2.py
@@ -4,3 +4,42 @@ from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import font
+class TextEditor(
+    tk.Toplevel
+):
+
+    def __init__(
+        self,
+        master=None,
+        file_path=None
+    ):
+
+        super().__init__(
+            master
+        )
+
+        self.file_path = file_path
+
+        self.modified = False
+
+        self.wrap = False
+
+        self.font_size = 13
+
+        self.editor_font = font.Font(
+            family="Menlo",
+            size=self.font_size
+        )
+
+        self.title(
+            "Nebula Text Editor"
+        )
+
+        self.geometry(
+            "1100x700"
+        )
+
+        self.minsize(
+            800,
+            500
+        )
