6344f2cd1293ba48c74b52df0edbb105807de14c
codesterDiv <180194888+codesterDiv@users.noreply.github.com>
2026-08-02 23:34:14 +0530

    modified:   import_test.py 	modified:   main.py 	renamed:    ui/ui.py -> ui/old_ui.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import os
 import core.config as config
 import core.database as database
-from ui.ui import Nebula
+from ui.old_ui import Nebula
 from core.clipboard_manager import ClipboardManager
 
 os.makedirs(config.VAULT_DIR,exist_ok=True)
