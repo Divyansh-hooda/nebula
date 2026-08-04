14d4366de4ab65a0e38c6ac45f7a437f7f96f08e
codesterDiv <180194888+codesterDiv@users.noreply.github.com>
2026-08-04 15:42:50 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import tkinter as tk
-import os
-import core.config as config
-import core.database as database
-from ui.legacy_ui import Nebula
-from core.clipboard_manager import ClipboardManager
+from ui.app import NebulaApp
 
-os.makedirs(config.VAULT_DIR,exist_ok=True)
-os.makedirs(config.LOG_DIR,exist_ok=True)
-database.initialize()
-clipboard = ClipboardManager()
-root=tk.Tk()
-app=Nebula(root, clipboard)
-root.mainloop()
+if __name__ == "__main__":
+    NebulaApp().run()
